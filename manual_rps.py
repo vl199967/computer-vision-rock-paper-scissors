f0e497a48a567b0bfd9f0ffd49446779c7a34be5
vl199967 <vl199967@gmail.com>
2022-11-24 16:02:44 +0000

    please.

--- manual_rps.py
+++ manual_rps.py
@@ -12,7 +12,7 @@ def get_user_choice():
 def get_winner(computer_choice,user_choice):
     if computer_choice == user_choice :
         print("It is a tie!")
-    if (computer_choice == "Paper" and user_choice == "scissor") or (computer_choice == "Rock" and user_choice == "paper") or (computer_choice == "Scissors" and user_choice == "rock"): 
+    if (computer_choice == "Paper" and user_choice == "Scissors") or (computer_choice == "Rock" and user_choice == "Paper") or (computer_choice == "Scissors" and user_choice == "Rock"): 
         print("You won ")
     else:
         print("You lost")                       
