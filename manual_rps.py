98358ffaf537cc209236618bbd23dc7106711976
vl199967 <vl199967@gmail.com>
2022-11-24 15:39:59 +0000

    implements game logic.

--- manual_rps.py
+++ manual_rps.py
@@ -2,7 +2,7 @@ import random
 from random import input
 
 def get_computer_choice():
-    options = ["Rock","Paper","Scissor","Nothing"]
+    options = ["rock","paper","scissor","nothing"]
     chose = random.choice(options)
     return chose 
 
@@ -10,4 +10,10 @@ def get_user_choice():
        user_choice = input()
        return user_choice
 
-       
+def get_winner(computer_choice,user_choice):
+    if computer_choice == user_choice :
+        print("It's a tie!")
+    if (computer_choice == "paper" and user_choice == "scissor") or (computer_choice == "rock" and user_choice == "paper") or (computer_choice == "scissor" and user_choice == "rock"): 
+        print("You won!")
+    else:
+        print("You lost!")                        
