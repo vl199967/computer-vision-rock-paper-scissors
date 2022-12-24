3a28cfff0d9367b42903f008c2a4cad78cf29885
vl199967 <vl199967@gmail.com>
2022-12-24 19:14:40 +0000

    implements countdown for RPS-template.

--- RPS-Template.py
+++ RPS-Template.py
@@ -1,11 +1,15 @@
 import cv2
 from keras.models import load_model
 import numpy as np
+from time import time
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-while True: 
+max_time = 5
+start = time()
+
+while (time() - start) < max_time: 
     ret, frame = cap.read()
     resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
     image_np = np.array(resized_frame)
