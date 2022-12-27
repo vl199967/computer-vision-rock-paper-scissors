4f9a2f5447040f540020a643946453d0b73bc51e
vl199967 <vl199967@gmail.com>
2022-12-27 19:48:47 +0000

    one more commit

--- camera_rps.py
+++ camera_rps.py
@@ -42,11 +42,33 @@ def get_computer_choice():
 
 def get_winner(computer_choice,user_choice):
     if (computer_choice == "Paper" and user_choice == 2) or (computer_choice == "Rock" and user_choice == 0) or (computer_choice == "Scissors" and user_choice == 1) :
-        print("It is a tie!")
+        return("It is a tie!")
     elif (computer_choice == "Paper" and user_choice == 1) or (computer_choice == "Rock" and user_choice == 2) or (computer_choice == "Scissors" and user_choice == 0): 
-        print("You won ")
+        return("You won")
     else:
-        print("You lost")                       
+        return("You lost")                       
+
+def play():
+    pwin = 0 
+    cwin = 0 
+    for i in range(3):
+      playa = get_prediction()
+      comp = get_computer_choice()
+      print("Player: {} Comp: {}".format(playa, comp))
+      res  = get_winner(comp,playa)
+      if res == "You won":
+        pwin = pwin + 1
+      elif res == "You lost":
+        cwin = cwin + 1  
+
+    if pwin > cwin:
+        print("Player won the game!")
+    elif pwin < cwin:
+        print("You lost the game!")  
+    else:
+        print("It is a tie!")          
+    
+play()
 
 
 
